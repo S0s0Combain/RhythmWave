b5933442aa539c49bdf4e962be3cac8ee70ea43e
Artem <kudrinskiy@internet.ru>
2024-10-24 18:31:29 +0300

    Реализована возможность поделиться музыкальным файлом

--- app/src/main/java/com/example/rhythmwave/TrackUtils.kt
+++ app/src/main/java/com/example/rhythmwave/TrackUtils.kt
@@ -5,25 +5,30 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.provider.MediaStore
+import androidx.core.content.FileProvider
 import java.io.File
 
 object TrackUtils {
-    fun shareTrack(context: Context, track: Track, contentResolver: ContentResolver){
+    fun shareTrack(context: Context, track: Track, contentResolver: ContentResolver) {
         val uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
         val selection = "${MediaStore.Audio.Media._ID} = ?"
         val selectionArgs = arrayOf(track.id.toString())
         val cursor = contentResolver.query(uri, null, selection, selectionArgs, null)
 
         cursor?.use {
-            if(it.moveToFirst()){
+            if (it.moveToFirst()) {
                 val filePath = it.getString(it.getColumnIndexOrThrow(MediaStore.Audio.Media.DATA))
                 val file = File(filePath)
                 val mimeType = "audio/mpeg"
+                val fileUri = FileProvider.getUriForFile(context, "${context.packageName}.provider", file)
+
                 val shareIntent = Intent(Intent.ACTION_SEND)
                 shareIntent.type = mimeType
-                shareIntent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile((file)))
-                context.startActivity(Intent.createChooser(shareIntent, "ShareTrack"))
+                shareIntent.putExtra(Intent.EXTRA_STREAM, fileUri)
+                shareIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+                context.startActivity(Intent.createChooser(shareIntent, "Share Track"))
             }
         }
     }
+
 }
